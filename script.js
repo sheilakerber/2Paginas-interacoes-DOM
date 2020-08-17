395d424078ecb3148ba3986a28893ac7bc379303
Sheila Kerber <sheilask.bio@gmail.com>
2020-08-17 16:13:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//verificar screen width, height, availWidth, e availHeight
+console.log("Screen width: " + screen.width)
+console.log("Screen avail width: " + screen.availWidth)
+console.log("Screen height: " + screen.height)
+console.log("Screen avail height: " + screen.availHeight)
+
+//verificar cookies enabled
+console.log("Cookies enabled: " + navigator.cookieEnabled)
